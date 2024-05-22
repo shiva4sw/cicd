0b5dd1e2029d005c958402dad22c5f43c06f2213
shiva4tr <115980750+shiva4tr@users.noreply.github.com>
2024-05-22 11:23:23 +0530

    commit2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Reva Univesity</h1>Azure Training'
+    return '<h1>Reva Univesity</h1>Azure Training...'
 
 if __name__=='__main__':
     app.run(debug=True)
