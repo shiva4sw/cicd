07955b705bc9ed67c53513d59f8e47ff549188ad
shiva4tr <115980750+shiva4tr@users.noreply.github.com>
2024-05-22 11:07:22 +0530

    comit1

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Reva Univesity</h1>Azure Training'
+
+if __name__=='__main__':
+    app.run(debug=True)
